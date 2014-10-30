25f0ca565f8770676a12c746575415246210aeb1
Nicolas Willis-Blunt <nicolas.wb@outlook.com>
2014-10-30 10:00:25 -0700

    Product controller update for image upload

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,17 @@ class ProductsController < ApplicationController
 		end
 	end
 
-	def create
-		if(Product.create(product_params).valid?)
+	def
+    create
+    product_params = JSON.parse(params[:product])
+
+    product = Product.create(product_params)
+    if params[:image]
+      product.image = params[:image][:image]
+      product.save
+    end
+
+		if product.valid?
 			respond_to do |format|
 				format.json {render status: :ok, nothing: true}
 				format.html {render status: :ok, nothing: true}
@@ -32,7 +41,7 @@ class ProductsController < ApplicationController
 	def show
 		@product = Product.find_by_id(params[:id])
 
-		if(@product.valid?)
+		if @product.valid?
 			respond_to do |format|
 				format.json {render :show}
 				format.html {render :show}
@@ -48,7 +57,7 @@ class ProductsController < ApplicationController
 	def update
 		@product = Product.find_by_id(params[:id])
 
-		@product.update_attributes(product_params);
+		@product.update_attributes(product_params)
 		render nothing: true
 	end
 private
